b7240021e1f2dd53b35e0bc61f047a5f4075fc14
yuta.itatsu <yuta.itatsu@gmail.com>
2025-06-10 23:16:41 +0900

    fixed

--- examples/run_example.py
+++ examples/run_example.py
@@ -11,9 +11,9 @@ here = Path(__file__).parent.resolve()
 # paths are relative to the current directory
 if __name__ == "__main__":
     pipeline = Pipeline.from_template_dir(
-        dirpath="./examples/config/templates",
-        param_path="./examples/config/params/params.yaml",
+        dirpath="./config/templates",
+        param_path="./config/params/params.yaml",
         derive_func_paths=["./custom_functions/derive_funcs.py"],
         transform_func_paths=["./custom_functions/transform_funcs.py"],
     )
-    pipeline.run(script_path="./scripts/generated_user_flow.py", execute=True, dag=True)
+    pipeline.run(execute=True, dag=True)
